425141167a36e48b66e1684c7424402a25892504
Karolgonzalez <gisellcastro83@gmail.com>
2022-02-03 23:55:26 -0600

    Agregamos una lista de producto

--- Quieropizza/QuieroPizza.Web/Controllers/HomeController.cs
+++ Quieropizza/QuieroPizza.Web/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace QuieroPizza.Web.Controllers
         // GET: Home
         public ActionResult Index()
         {
-            return View();
+            return View(); 
         }
     }
 }
